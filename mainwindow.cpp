fa7e0ae6a9ab0bb1ce022d6cf826b12743c22871
NeilKID <zgsxllwslyh@163.com>
2021-04-14 16:01:49 +0800

    add new project

---
+++ mainwindow.cpp
@@ -0,0 +1,321 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //连接读取串口信号和槽
+    QObject::connect(&serial, &QSerialPort::readyRead, this, &MainWindow::serialPort_readyRead);
+    //发送按键失能
+    ui->sendButton->setEnabled(false);
+    //波特率默认选择下拉第三项：9600
+    ui->baudrateBox->setCurrentIndex(3);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//读取串口数据
+void MainWindow::serialPort_readyRead()
+{
+    //从接收缓冲区中读取数据
+    QByteArray buffer = serial.readAll();
+    QString recv = "#";
+//    QString recv = buffer.toHex(':').toUpper().data();//测试是否接收数据
+
+    //初始标识符识别
+    for (int count = 0; count < buffer.length();count++)
+    {
+        if (buffer[count] == '#')
+        {
+            //接收4字节经度数据并转为float数据（保留2位小数）显示
+            char lonCh[4];
+            for(int i=0;i<4;i++)
+            {
+                lonCh[i] = buffer[i+1];
+            }
+            float lonFl = 0.0;
+            recv_float_char(lonFl,lonCh);
+            recv.append(QString::number(lonFl,'f',2));
+            recv.append(' ');
+            //接收4字节经度数据并转为float数据（保留2位小数）显示
+            char latCh[4];
+            for(int i=0;i<4;i++)
+            {
+                latCh[i] = buffer[i+5];
+            }
+            float latFl = 0.0;
+            recv_float_char(latFl,latCh);
+            recv.append(QString::number(latFl,'f',2));
+            recv.append(' ');
+            //接收2字节航向角并转为int数据显示
+            char yawCh[2];
+            for(int i=0;i<2;i++)
+            {
+                yawCh[i] = buffer[i+9];
+            }
+            unsigned short yawUS = 0;
+            recv_ushort_char(yawUS,yawCh);
+            recv.append(QString::number(yawUS));
+            recv.append(' ');
+            //接收2字节速度并转为int数据显示
+            char vecCh[2];
+            for(int i=0;i<2;i++)
+            {
+                vecCh[i] = buffer[i+11];
+            }
+            unsigned short vecUS = 0;
+            recv_ushort_char(vecUS,vecCh);
+            recv.append(QString::number(vecUS));
+            recv.append(' ');
+            //接收1字节定位模式并以字符串显示
+            char locMoCh = buffer[13];
+            recv.append(locMoCh);
+        }
+    }
+
+    //从界面中读取以前收到的数据
+    ui->recvTextEdit->append(recv);
+
+    //获得接收串口读取的ByteArray数据长度，存储为int，以字符串形式显示
+    int recvLengthNum = buffer.size();
+//    int recvLengthNum = sizeof(short); //测试short类型占用字节
+    QString recvLength = QString::number(recvLengthNum);
+    ui->recvTextLength->append(recvLength);
+}
+
+//点击检测串口信息，显示目前可用串口
+void MainWindow::on_searchButton_clicked()
+{
+    ui->portNameBox->clear();
+    QList<QSerialPortInfo> strlist=QSerialPortInfo::availablePorts();
+    QList<QSerialPortInfo>::const_iterator iter;
+    for(iter=strlist.constBegin();iter!=strlist.constEnd();++iter)
+    {
+        ui->portNameBox->addItem((*iter).portName());
+    }
+}
+
+//点击打开串口
+void MainWindow::on_openButton_clicked()
+{
+    if(ui->openButton->text()==QString::fromUtf8("打开串口"))
+    {
+        //设置串口名
+        serial.setPortName(ui->portNameBox->currentText());
+        //设置波特率
+        serial.setBaudRate(ui->baudrateBox->currentText().toInt());
+        //设置数据位数
+        switch(ui->dataBitsBox->currentText().toInt())
+        {
+        case 8:
+            serial.setDataBits(QSerialPort::Data8);
+            break;
+        case 7:
+            serial.setDataBits(QSerialPort::Data7);
+            break;
+        case 6:
+            serial.setDataBits(QSerialPort::Data6);
+            break;
+        case 5:
+            serial.setDataBits(QSerialPort::Data5);
+            break;
+        default:
+            break;
+        }
+        //设置奇偶校验
+        switch(ui->parityBox->currentIndex())
+        {
+        case 0:
+            serial.setParity(QSerialPort::NoParity);
+            break;
+        case 1:
+            serial.setParity(QSerialPort::EvenParity);
+            break;
+        case 2:
+            serial.setParity(QSerialPort::OddParity);
+            break;
+        default:
+            break;
+        }
+        //设置停止位
+        switch(ui->stopBitsBox->currentIndex())
+        {
+        case 0:
+            serial.setStopBits(QSerialPort::OneStop);
+            break;
+        case 1:
+            serial.setStopBits(QSerialPort::TwoStop);
+        case 2:
+            serial.setStopBits(QSerialPort::OneAndHalfStop);
+            break;
+        default: break;
+        }
+        //设置流控制
+        switch (ui->flowBox->currentIndex())
+        {
+        case 0:
+            serial.setFlowControl(QSerialPort::NoFlowControl);
+            break;
+        case 1:
+            serial.setFlowControl(QSerialPort::HardwareControl);
+            break;
+        case 2:
+            serial.setFlowControl(QSerialPort::SoftwareControl);
+            break;
+        }
+        //打开串口
+        if(!serial.open(QIODevice::ReadWrite))
+        {
+           QMessageBox::about(NULL, QString::fromUtf8("提示 "), QString::fromUtf8("无法打开串口！"));
+           return;
+        }
+        //下拉菜单控件失能
+        ui->portNameBox->setEnabled(false);
+        ui->baudrateBox->setEnabled(false);
+        ui->dataBitsBox->setEnabled(false);
+        ui->parityBox->setEnabled(false);
+        ui->stopBitsBox->setEnabled(false);
+        ui->flowBox->setEnabled(false);
+        ui->openButton->setText(QString::fromUtf8("关闭串口"));
+        //发送按键使能
+        ui->sendButton->setEnabled(true);
+    }
+    else
+    {
+        //关闭串口
+        serial.close();
+        //下拉菜单控件使能
+        ui->portNameBox->setEnabled(true);
+        ui->baudrateBox->setEnabled(true);
+        ui->dataBitsBox->setEnabled(true);
+        ui->parityBox->setEnabled(true);
+        ui->stopBitsBox->setEnabled(true);
+        ui->flowBox->setEnabled(true);
+        ui->openButton->setText(QString::fromUtf8("打开串口"));
+        //发送按键失能
+        ui->sendButton->setEnabled(false);
+    }
+}
+
+//点击发送数据
+void MainWindow::on_sendButton_clicked()
+{
+    //从编辑栏读取用户写入的转角与速度，分别存为unsigned short与float
+    QString strSendSw = ui->sendSwTextEdit->toPlainText();
+    QString strSendVec = ui->sendVecTextEdit->toPlainText();
+
+    //发送数据包序列号
+    int sequIn = 1+48 ;
+    char sequCh[1];
+    sequCh[0] = (char)sequIn;
+    QByteArray sequBa = QByteArray(sequCh,1);
+
+    //将unsigned short转角转为2字节的char数组
+    short swSeSh = strSendSw.toShort();
+    char swSeCh[2];
+    send_short_char(swSeSh,swSeCh);
+    QByteArray swSeBa = QByteArray(swSeCh,2);
+
+//    //将float速度转为4字节的char数组
+//    float vecSeFl = strSendVec.toFloat();
+//    char vecSeCh[4];
+//    send_float_char(vecSeFl,vecSeCh);
+//    QByteArray vecSeBa = QByteArray(vecSeCh,4);
+
+    //将unsigned short速度转为2节的char数组
+    unsigned short vecSeUS = strSendVec.toUShort();
+    char vecSeCh[2];
+    send_ushort_char(vecSeUS,vecSeCh);
+    QByteArray vecSeBa = QByteArray(vecSeCh,2);
+
+    //将1字节起始符、转角2字节、速度4字节写入发送ByteArray字节串
+    QByteArray sendData;
+    sendData.append("#");
+    sendData.append(sequBa);
+    sendData.append(swSeBa);
+    sendData.append(vecSeBa);
+
+    //将ByteArray数据写入串口发送
+    serial.write(sendData);
+
+    //获得发送串口的ByteArray数据长度，存储为int，以字符串形式显示
+    int sendLengthNum = sendData.size();
+//    int sendLengthNum = sizeof (inSData);
+    QString sendLength = QString::number(sendLengthNum);
+    ui->sendTextLength->append(sendLength);
+}
+
+//点击清除接收区显示数据
+void MainWindow::on_clearButton_clicked()
+{
+    ui->recvTextEdit->clear();
+}
+
+//发送数据float转char函数
+void MainWindow::send_float_char(float data, char *s)
+{
+    sendUnion.ff = data;
+    for(int i=0;i<4;i++)
+    {
+        s[i] = sendUnion.ch[i];
+    }
+}
+
+//发送数据unsigned short转char函数
+void MainWindow::send_ushort_char(unsigned short data, char *s)
+{
+    sendUnion.us = data;
+    for(int i=0;i<2;i++)
+    {
+        s[i] = sendUnion.ch[i];
+    }
+}
+
+//发送数据short转char函数
+void MainWindow::send_short_char(short data, char *s)
+{
+    sendUnion.ss = data;
+    for(int i=0;i<2;i++)
+    {
+        s[i] = sendUnion.ch[i];
+    }
+}
+
+//接收数据char转float函数
+void MainWindow::recv_float_char(float &data, char *s)
+{
+    for(int i=0;i<4;i++)
+    {
+        recvUnion.ch[i] = s[i];
+    }
+    data = recvUnion.ff;
+}
+
+
+//接收数据char转unsigned short函数
+void MainWindow::recv_ushort_char(unsigned short &data, char *s)
+{
+    for(int i=0;i<2;i++)
+    {
+        recvUnion.ch[i] = s[i];
+    }
+    data = recvUnion.us;
+}
+
+//接收数据char转short函数
+void MainWindow::recv_short_char(short &data, char *s)
+{
+    for(int i=0;i<2;i++)
+    {
+        recvUnion.ch[i] = s[i];
+    }
+    data = recvUnion.ss;
+}
---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>        //提供访问串口的功能
+#include <QSerialPortInfo>    //提供系统中存在的串口的信息
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort serial;
+    //定义int、float与char数组共同体
+    union myUnion
+    {
+        float ff;
+        int ii;
+        unsigned short us;
+        short ss;
+        char ch[4];
+    }sendUnion,recvUnion;
+    void send_float_char(float data, char *s);
+    void send_ushort_char(unsigned short data, char *s);
+    void send_short_char(short data, char *s);
+    void recv_float_char(float &data, char *s);
+    void recv_ushort_char(unsigned short &data, char *s);
+    void recv_short_char(short &data, char *s);
+
+private slots:
+    void serialPort_readyRead();
+    void on_searchButton_clicked();
+    void on_openButton_clicked();
+    void on_sendButton_clicked();
+    void on_clearButton_clicked();
+
+};
+#endif // MAINWINDOW_H
+
+
